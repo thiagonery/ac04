54369ac0f6e43eca7fd7b3163d5861229a78d5b5
thiagonery <60719065+thiagonery@users.noreply.github.com>
2021-05-23 21:59:48 -0300

    Update ac.py

--- ac.py
+++ ac.py
@@ -5,50 +5,50 @@ import random
 
 app = Flask(__name__)
 
-disciplinas = [{"id": e, "nome": "Disciplina "+str(e), "ementa":"Ementa "+str(e), "foto":"https://cdn.pixabay.com/photo/2018/01/18/20/42/pencil-3091204_1280.jpg", "professor": "Professor Disciplina "+str(e)} for e in range(1,11)]   
+produtos = [{"id": e, "nome": "Produto "+str(e), "Descricao ":"Descricao "+str(e), "foto":"https://cdn.pixabay.com/photo/2018/01/18/20/42/pencil-3091204_1280.jpg", "Responsavel": "Funcionario "+str(e)} for e in range(1,11)]
 
-@app.route("/disciplinas", methods=['GET'])
+@app.route("/produtos", methods=['GET'])
 def get():
-    return jsonify(disciplinas)
+    return jsonify(produtos)
 
-@app.route("/disciplinas/<int:id>", methods=['GET'])
+@app.route("/produtos/<int:id>", methods=['GET'])
 def get_one(id):
-    filtro = [e for e in disciplinas if e["id"] == id]
+    filtro = [e for e in produtos if e["id"] == id]
     if filtro:
         return jsonify(filtro[0])
     else:
         return jsonify({})
 
-@app.route("/disciplinas", methods=['POST'])
+@app.route("/produtos", methods=['POST'])
 def post():
-    global disciplinas
+    global produtos
     try:
         content = request.get_json()
 
         # gerar id
-        ids = [e["id"] for e in disciplinas]
+        ids = [e["id"] for e in produtos]
         if ids:
             nid = max(ids) + 1
         else:
             nid = 1
         content["id"] = nid
-        disciplinas.append(content)
-        return jsonify({"status":"OK", "msg":"disciplina adicionada com sucesso"})
+        produtos.append(content)
+        return jsonify({"status":"OK", "msg":"produto adicionado com sucesso"})
     except Exception as ex:
         return jsonify({"status":"ERRO", "msg":str(ex)})
 
-@app.route("/disciplinas/<int:id>", methods=['DELETE'])
+@app.route("/produtos/<int:id>", methods=['DELETE'])
 def delete(id):
-    global disciplinas
+    global produtos
     try:
-        disciplinas = [e for e in disciplinas if e["id"] != id]
-        return jsonify({"status":"OK", "msg":"disciplina removida com sucesso"})
+        produtos = [e for e in produtos if e["id"] != id]
+        return jsonify({"status":"OK", "msg":"produto adicionado com sucesso"})
     except Exception as ex:
         return jsonify({"status":"ERRO", "msg":str(ex)})
 
 @app.route("/push/<string:key>/<string:token>", methods=['GET'])
 def push(key, token):
-	d = random.choice(disciplinas)
+	d = random.choice(produtos)
 	data = {
 		"to": token,
 		"notification" : {
